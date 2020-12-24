4fc1369c62d71d4a66564afa10ff1e45f218bcf0
shyam-22 <shyambadgujar95@gmail.com>
2020-12-24 11:32:23 +0530

    #shyamm : semi_update

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,11 +1,32 @@
+import React, { Component } from "react"
 import "./Style Folder/App.css";
+import quizdata from "./QuizData"
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Quiz App</h1>
-    </div>
-  );
+class App extends Component{
+  state={
+    questionBank : []
+  };
+
+  getQuestions = () => {
+    quizdata().then(question =>{
+      this.setState({questionBank:question})
+    })
+  }
+  componentDidMount(){
+    this.getQuestions()
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <div className="title">Quiz App</div>
+        {
+      this.state.questionBank.length > 0 && this.state.questionBank.map(({
+        question,answers,correct,questionId})=> (<h4>{question}</h4>) 
+        )}
+      </div>
+    ); 
+  }
 }
 
 export default App;
